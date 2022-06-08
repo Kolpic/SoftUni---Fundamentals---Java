484dfedb0ae0c8fe3262829940827e7636bd9f13
Galin Petrov <106113997+Kolpic@users.noreply.github.com>
2022-06-08 22:47:56 +0300

    Add files via upload

---
+++ Lab_Arrays/SumEvenNumbers_03.java
@@ -0,0 +1,27 @@
+package Lab_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumEvenNumbers_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int[] numbers = Arrays
+                .stream(input.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int currentNumber = numbers[i];
+            if (currentNumber % 2 == 0){
+                sum += currentNumber;
+            }
+        }
+        System.out.println(sum);
+    }
+}
