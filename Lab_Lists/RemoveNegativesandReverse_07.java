d95e24f665115d559187555bc0855865257acaff
Kolpic <galincho112@gmail.com>
2022-06-24 15:45:21 +0300

    Added task

---
+++ Lab_Lists/RemoveNegativesandReverse_07.java
@@ -0,0 +1,27 @@
+package Lab_Lists;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class RemoveNegativesandReverse_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        numbers.removeIf(e -> e < 0);
+
+        if (numbers.isEmpty()){
+            System.out.println("empty");
+        }else {
+            Collections.reverse(numbers);
+            for (int num : numbers) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
